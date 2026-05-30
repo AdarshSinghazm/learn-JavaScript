bc13e3cb130c5b09eaaf1d7685e959d266881c12
Adarsh Singh <adarshsinghsinghpurr@gmail.com>
2026-05-30 13:51:02 +0530

    Handling of objects and Arrays in functions

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -1,34 +1,122 @@
-// Rest operator
-// so basically in real world when u are working on a project... lets say u are making a ecommerce website.. and u have to calculate cart price.. u dont know how many item customer will add in cart .. so to hadle this--> Rest Operator
- 
-function calculateCartPrice(...nums){
-        return nums; // this will return a array
+
+// ======================
+// REST OPERATOR (...)
+// ======================
+
+// Real world use case:
+//
+// Suppose you are building an e-commerce website.
+// You need to calculate cart total price.
+//
+// Problem:
+// You don't know how many items
+// the customer will add to cart.
+//
+// To handle variable number of arguments,
+// we use Rest Operator (...)
+
+
+function calculateCartPrice(...nums) {
+
+    // Rest operator collects all values
+    // into an array
+
+    return nums;
 }
-console.log(calculateCartPrice(200,300,400,500))
- 
-function calculateCartPrice2(val1,val2,...nums){
-        return `val1 : ${val1}, val2 : ${val2}, nums : ${nums}`; // rest will go in nums
+
+console.log(calculateCartPrice(200, 300, 400, 500));
+
+
+// Output:
+// [200, 300, 400, 500]
+
+
+// ======================
+// REST WITH NORMAL PARAMETERS
+// ======================
+
+function calculateCartPrice2(val1, val2, ...nums) {
+
+    // First two values go into:
+    // val1 and val2
+
+    // Remaining values go into nums
+
+    return `val1 : ${val1}, val2 : ${val2}, nums : ${nums}`;
 }
-console.log(calculateCartPrice2(200,300,400,500))
+
+console.log(calculateCartPrice2(200, 300, 400, 500));
 
 
-// way to handle object in JS
- user ={
-    name:"Adarsh",
-    price:"2000"
- }
- // TYpe safety is necesssary so.. in functionn u always check for type safety that object u are calling is of object or not.. properies u want to access is prest in that object or not.. all that thing.. in Type script these things are inbuilt..we will handle all this when we leanr conditonals and loops..
+// Output:
+// val1 : 200
+// val2 : 300
+// nums : 400,500
 
- function objectHandle(anyObject){
-     console.log(`Name is ${anyObject.name} and Price is ${anyObject.price}`);
- }
- objectHandle(user)
 
- // array passed in functions
-const ar = [1,2,3,4,5]
- function findSecondElement(getArray){
+// ======================
+// PASSING OBJECTS TO FUNCTIONS
+// ======================
+
+// In real projects,
+// functions often receive objects.
+
+const user = {
+    name: "Adarsh",
+    price: 2000
+};
+
+
+// Type safety is important.
+//
+// Before accessing object properties,
+// make sure:
+// - argument is actually an object
+// - required properties exist
+//
+// TypeScript helps with this automatically.
+
+function objectHandle(anyObject) {
+
+    console.log(
+        `Name is ${anyObject.name} and Price is ${anyObject.price}`
+    );
+}
+
+objectHandle(user);
+
+
+// We can also directly pass object
+
+objectHandle({
+    name: "Rishu",
+    price: 5000
+});
+
+
+// ======================
+// PASSING ARRAYS TO FUNCTIONS
+// ======================
+
+const ar = [1, 2, 3, 4, 5];
+
+function findSecondElement(getArray) {
+
     console.log(getArray[1]);
- }
- findSecondElement(ar) 
+}
 
- // u can directly pass array and object without storing them in a variable
+findSecondElement(ar);
+
+
+// Directly passing array
+
+findSecondElement([10, 20, 30, 40]);
+
+
+// ======================
+// IMPORTANT NOTES
+// ======================
+
+// ...nums
+// -> Rest Operator
+// -> Collects multiple values into array
